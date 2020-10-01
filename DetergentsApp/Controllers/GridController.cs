bebc131b85c3319ca6823f00f76c251d31268d60
Eric Volmer <eric.volmer@hotmail.com>
2020-10-01 14:07:43 +0200

    Working Add, Edit and Delete

--- DetergentsApp/Controllers/GridController.cs
+++ DetergentsApp/Controllers/GridController.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using DetergentsApp.Models;
 using Kendo.Mvc.Extensions;
-using Kendo.Mvc.UI;
 
 namespace DetergentsApp.Controllers
 {
@@ -27,12 +25,23 @@ namespace DetergentsApp.Controllers
 
         public IEnumerable<Product> Read()
         {
-            
-            
-           return db.Products;
+            return db.Products;
         }
 
-        
+        public void Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            product.ProductID = product.ProductID;
+        }
+
+        public void Update(Product product)
+        {
+            db.Products.Attach(product);
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+        }
 
         public void Destroy(Product product)
         {
@@ -51,204 +60,7 @@ namespace DetergentsApp.Controllers
             db.Products.Remove(entity);
             db.SaveChanges();
         }
-    
-        
-    
-     public ProductService(DetergentsEntities entities)
-        {
-            this.entities = entities;
-        }
-
-        public IList<ProductViewModel> GetAll()
-        {
-            var result = HttpContext.Current.Session["Products"] as IList<ProductViewModel>;
-
-            if (result == null || UpdateDatabase)
-            {
-                result = entities.Products.Select(product => new ProductViewModel
-                {
-                    ProductID = product.ProductID,
-                    ProductName = product.ProductName,
-                    UnitPrice = product.UnitPrice.HasValue ? product.UnitPrice.Value : default(decimal),
-                    UnitsInStock = product.UnitsInStock.HasValue ? product.UnitsInStock.Value : default(short),
-                    QuantityPerUnit = product.QuantityPerUnit,
-                    Discontinued = product.Discontinued,
-                    UnitsOnOrder = product.UnitsOnOrder.HasValue ? (int)product.UnitsOnOrder.Value : default(int),
-                    CategoryID = product.CategoryID,
-                    Category = new CategoryViewModel()
-                    {
-                        CategoryID = product.Category.CategoryID,
-                        CategoryName = product.Category.CategoryName
-                    },
-                    LastSupply = DateTime.Today
-                }).ToList();
-
-                HttpContext.Current.Session["Products"] = result;
-            }
-
-            return result;
-        }
-
-        public IEnumerable<ProductViewModel> Read()
-        {
-            return GetAll();
-        }
-
-        public void Create(ProductViewModel product)
-        {
-            if (!UpdateDatabase)
-            {
-                var first = GetAll().OrderByDescending(e => e.ProductID).FirstOrDefault();
-                var id = (first != null) ? first.ProductID : 0;
-
-                product.ProductID = id + 1;
-
-                if (product.CategoryID == null)
-                {
-                    product.CategoryID = 1;
-                }
-
-                if (product.Category == null)
-                {
-                    product.Category = new CategoryViewModel() { CategoryID = 1, CategoryName = "Beverages" };
-                }
-
-                GetAll().Insert(0, product);
-            }
-            else
-            {
-                var entity = new Product();
-
-                entity.ProductName = product.ProductName;
-                entity.UnitPrice = product.UnitPrice;
-                entity.UnitsInStock = (short)product.UnitsInStock;
-                entity.Discontinued = product.Discontinued;
-                entity.CategoryID = product.CategoryID;
-
-                if (entity.CategoryID == null)
-                {
-                    entity.CategoryID = 1;
-                }
-
-                if (product.Category != null)
-                {
-                    entity.CategoryID = product.Category.CategoryID;
-                }
-
-                entities.Products.Add(entity);
-                entities.SaveChanges();
-
-                product.ProductID = entity.ProductID;
-            }
-        }
-
-        public void Update(ProductViewModel product)
-        {
-            if (!UpdateDatabase)
-            {
-                var target = One(e => e.ProductID == product.ProductID);
-
-                if (target != null)
-                {
-                    target.ProductName = product.ProductName;
-                    target.UnitPrice = product.UnitPrice;
-                    target.UnitsInStock = product.UnitsInStock;
-                    target.Discontinued = product.Discontinued;
-
-                    if (product.CategoryID == null)
-                    {
-                        product.CategoryID = 1;
-                    }
-
-                    if (product.Category != null)
-                    {
-                        product.CategoryID = product.Category.CategoryID;
-                    }
-                    else
-                    {
-                        product.Category = new CategoryViewModel()
-                        {
-                            CategoryID = (int)product.CategoryID,
-                            CategoryName = entities.Categories.Where(s => s.CategoryID == product.CategoryID).Select(s => s.CategoryName).First()
-                        };
-                    }
-                    
-                    target.CategoryID = product.CategoryID;
-                    target.Category = product.Category;
-                }
-            }
-            else
-            {
-                var entity = new Product();
-
-                entity.ProductID = product.ProductID;
-                entity.ProductName = product.ProductName;
-                entity.UnitPrice = product.UnitPrice;
-                entity.UnitsInStock = (short)product.UnitsInStock;
-                entity.Discontinued = product.Discontinued;
-                entity.CategoryID = product.CategoryID;
-
-                if (product.Category != null)
-                {
-                    entity.CategoryID = product.Category.CategoryID;
-                }
-
-                entities.Products.Attach(entity);
-                entities.Entry(entity).State = EntityState.Modified;
-                entities.SaveChanges();
-            }
-        }
-
-        public void Destroy(ProductViewModel product)
-        {
-            if (!UpdateDatabase)
-            {
-                var target = GetAll().FirstOrDefault(p => p.ProductID == product.ProductID);
-                if (target != null)
-                {
-                    GetAll().Remove(target);
-                }
-            }
-            else
-            {
-                var entity = new Product();
-
-                entity.ProductID = product.ProductID;
-
-                entities.Products.Attach(entity);
-
-                entities.Products.Remove(entity);
-
-                var orderDetails = entities.Order_Details.Where(pd => pd.ProductID == entity.ProductID);
-
-                foreach (var orderDetail in orderDetails)
-                {
-                    entities.Order_Details.Remove(orderDetail);
-                }
-
-                entities.SaveChanges();
-            }
-        }
-
-        public ProductViewModel One(Func<ProductViewModel, bool> predicate)
-        {
-            return GetAll().FirstOrDefault(predicate);
-        }
-
-        public void Dispose()
-        {
-            entities.Dispose();
-        }
     }
-    
-    
-    
-    
-    
-    
-    /// <summary>
-    /// ////////////////////////////////////////////////////////////////////////
-    /// </summary>
 
     public class GridController : Controller
     {
@@ -258,20 +70,6 @@ namespace DetergentsApp.Controllers
         public GridController()
         {
             productService = new ProductService(new DetergentsEntities());
-        }
-        public ActionResult Orders_Read([DataSourceRequest]DataSourceRequest request)
-        {
-            var result = db.Products;
-            var result = Enumerable.Range(0, 50).Select(i => new Product()
-            {
-                ProductID = 
-                Freight = i * 10,
-                OrderDate = DateTime.Now.AddDays(i),
-                ShipName = "ShipName " + i,
-                ShipCity = "ShipCity " + i
-            });
-
-            return Json(result.ToDataSourceResult(request));
         }
 
         protected override void Dispose(bool disposing)
@@ -289,23 +87,20 @@ namespace DetergentsApp.Controllers
             ViewData["categories"] = categories;
             return View(products);
         }
-        public ActionResult Products_Create()
-        {
-            return Grid();
-        }
+
 
         [AcceptVerbs(HttpVerbs.Post)]
         public ActionResult Products_Create(Product product)
-        
+
         {
             if (ModelState.IsValid)
             {
-                db.Products.Add(product);
-                db.SaveChanges();
-                return RedirectToAction("Grid");
+                productService.Create(product);
+
+                var routeValues = this.GridRouteValues();
+                return RedirectToAction("Grid", routeValues);
             }
 
-            ViewBag.Category = new SelectList(db.Categories, "CategoryID", "CategoryName", product.Category);
             return View("Grid", productService.Read());
         }
 
@@ -314,9 +109,10 @@ namespace DetergentsApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Grid");
+                productService.Update(product);
+
+                var routeValues = this.GridRouteValues();
+                return RedirectToAction("Grid", routeValues);
             }
 
             return View("Grid", productService.Read());
