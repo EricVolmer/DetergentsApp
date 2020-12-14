1f10a71af143aef6ad30f4bae1b074dc6633dd0f
Eric Volmer <37138635+EricVolmer@users.noreply.github.com>
2020-12-14 11:49:29 +0100

    new view?

---
+++ DetergentsApp/Controllers/VendorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DetergentsApp.Models;
+
+namespace DetergentsApp.Controllers
+{
+    public class VendorController : Controller
+    {
+        private DetergentsEntities db = new DetergentsEntities();
+
+        // GET: Vendor
+        public ActionResult Index()
+        {
+            return View(db.VendorSet.ToList());
+        }
+
+    }
+}
