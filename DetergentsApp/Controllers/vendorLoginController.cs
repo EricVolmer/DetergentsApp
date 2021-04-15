9e86af67bd1003eb593389b8f3e2c99e93427382
Eric Volmer <37138635+EricVolmer@users.noreply.github.com>
2021-04-15 22:33:49 +0200

    Vendor Login optimized and code ready for displaying the right menu tabs for spesific users

--- DetergentsApp/Controllers/vendorLoginController.cs
+++ DetergentsApp/Controllers/vendorLoginController.cs
@@ -2,6 +2,7 @@
 using System.Security.Cryptography;
 using System.Text;
 using System.Web.Mvc;
+using System.Web.Security;
 using DetergentsApp.Models;
 
 namespace DetergentsApp.Controllers
@@ -10,6 +11,7 @@ namespace DetergentsApp.Controllers
     {
         private readonly DetergentsEntities db = new DetergentsEntities();
 
+        // [Authorize(Roles = "Admin")]
         // GET
         public ActionResult Index()
         {
@@ -47,38 +49,85 @@ namespace DetergentsApp.Controllers
             return View();
         }
 
-        public ActionResult vendorLogin()
-        {
-            return View();
-        }
-
+        // public ActionResult vendorLogin()
+        // {
+        //     return View();
+        // }
+        //
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public ActionResult vendorLogin(string userName, string password)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         var f_password = GetMD5(password);
+        //         var data = db.vendorLogin.Where(s => s.userName.Equals(userName) && s.password.Equals(f_password))
+        //             .ToList();
+        //         if (data.Count() > 0)
+        //         {
+        //             //add session
+        //             FormsAuthentication.SetAuthCookie(.userName, false); // set the formauthentication cookie  
+        //
+        //             Session["UserName"] = data.FirstOrDefault().userName;
+        //             Session["Id"] = data.FirstOrDefault().Id;
+        //             return RedirectToAction("Index");
+        //         }
+        //
+        //         ViewBag.error = "Login failed";
+        //          return RedirectToAction("vendorLogin");
+        //     }
+        //
+        //     return View();
+        // }
+        
+         public ActionResult vendorLogin()
+         {
+             return View();
+         }
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult vendorLogin(string userName, string password)
+        public ActionResult vendorLogin(vendorLogin _login, string userName, string password)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) //validating the user inputs  
             {
                 var f_password = GetMD5(password);
-                var data = db.vendorLogin.Where(s => s.userName.Equals(userName) && s.password.Equals(f_password))
-                    .ToList();
-                if (data.Count() > 0)
+                var isExist = false;
+                using (var _entity = new DetergentsEntities()) // out Entity name is "SampleMenuMasterDBEntites"  
                 {
-                    //add session
-                    Session["UserName"] = data.FirstOrDefault().userName;
-                    Session["Id"] = data.FirstOrDefault().Id;
-                    return RedirectToAction("Index");
+                    isExist = _entity.vendorLogin
+                        .Any(x => x.userName.Trim().ToLower() == _login.userName.Trim().ToLower() && x.password == f_password); //validating the user name in tblLogin table whether the user name is exist or not  
+                    if (isExist)
+                    {
+                        var _loginCredentials = _entity.vendorLogin.ToList()
+                            .Where(x => x.userName.Trim().ToLower() == _login.userName.Trim().ToLower()).Select(x =>
+                                new vendorLogin()
+                                {
+                                    userName = x.userName,
+                                    Id = x.Id,
+                                    password = x.password
+                                    
+                                }).FirstOrDefault(); // Get the login user details and bind it to LoginModels class  
+                        //Get the Menu details from entity and bind it in MenuModels list.  
+                        FormsAuthentication.SetAuthCookie(_loginCredentials.userName,
+                            false); // set the formauthentication cookie  
+                        Session["LoginCredentials"] = _loginCredentials; // Bind the _logincredentials details to "LoginCredentials" session  
+                        Session["UserName"] = _loginCredentials.userName;
+                        return RedirectToAction("Public", "Public");
+                    }
+                    ViewBag.ErrorMsg = "Please enter the valid credentials!...";
+                    return View();
                 }
-
-                ViewBag.error = "Login failed";
-                // return RedirectToAction("vendorLogin");
             }
-
             return View();
         }
 
-        //Logout
+//Logout
         public ActionResult Logout()
         {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            Session.RemoveAll();
             Session.Clear(); //remove session
             return RedirectToAction("vendorLogin");
         }
