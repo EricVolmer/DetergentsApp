a9f3744e3123d0573c26a64b4cc184e912fc7ab2
Eric Volmer <37138635+EricVolmer@users.noreply.github.com>
2021-04-29 10:32:02 +0200

    Categories optimized

--- DetergentsApp/Controllers/CategoriesController.cs
+++ DetergentsApp/Controllers/CategoriesController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Data.Entity;
-using System.Data.Entity.Validation;
-using System.Linq;
-using System.Net;
 using System.Web.Mvc;
 using DetergentsApp.Models;
+using Kendo.Mvc.UI;
 
 namespace DetergentsApp.Controllers
 {
@@ -12,121 +9,31 @@ namespace DetergentsApp.Controllers
     {
         private readonly DetergentsEntities db = new DetergentsEntities();
 
-        // GET: Categories
         public ActionResult Index()
-        {
-            return View(db.Categories.ToList());
-        }
-
-        // GET: Categories/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var category = db.Categories.Find(id);
-            if (category == null) return HttpNotFound();
-            return View(category);
-        }
-
-        // GET: Categories/Create
-        public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Categories/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "categoryName")] Category category)
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult CategoryCreate([DataSourceRequest] DataSourceRequest request,
+            ProductViewModel category)
         {
+            var newCategory = new Category()
+            {
+                categoryName = category.categoryName
+            };
             try
             {
-                if (ModelState.IsValid)
-                {
-                    category.categoryID = 0;
-                    db.Categories.Add(category);
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-
-                return View(category);
+                db.Categories.Add(newCategory);
+                db.SaveChanges();
             }
-            catch (DbEntityValidationException e)
+            catch (Exception e)
             {
-                foreach (var eve in e.EntityValidationErrors)
-                {
-                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
-                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
-                    foreach (var ve in eve.ValidationErrors)
-                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
-                            ve.PropertyName, ve.ErrorMessage);
-                }
-
+                Console.WriteLine(e);
                 throw;
             }
-        }
 
-        // GET: Categories/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var category = db.Categories.Find(id);
-            if (category == null) return HttpNotFound();
-            return View(category);
-        }
-
-        // POST: Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "categoryID,categoryName")]
-            Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(category).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(category);
-        }
-
-        // GET: Categories/Delete/5
-
-        public ActionResult Delete(int? id)
-        {
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var category = db.Categories.Find(id);
-            if (category == null) return HttpNotFound();
-
-            // How to find the products inside the category to delete them also.
-            //      var product = db.Products.Find(id);
-            //      if (product == null) return HttpNotFound();
-            return View(category);
-        }
-
-        // POST: Categories/Delete/5
-        [HttpPost]
-        [ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            var category = db.Categories.Find(id);
-            // How to find the products inside the category to delete them also.
-            //   var product = db.Products.Find(id);
-            db.Categories.Remove(category);
-            //   db.Products.Remove(product);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing) db.Dispose();
-            base.Dispose(disposing);
+            return View("Index");
         }
     }
 }
